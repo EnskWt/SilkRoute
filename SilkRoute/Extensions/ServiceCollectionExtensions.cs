85910753ffad604eee19883462817e21ca092785
MSI <MSI@DESKTOP-P1GRIBM>
2025-05-02 14:17:55 +0200

    Initial commit

---
+++ SilkRoute/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using SilkRoute.Interfaces;
+using SilkRoute.Proxy;
+using SilkRoute.Settings;
+
+namespace SilkRoute.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddMicroserviceClient<TClient>(
+            this IServiceCollection services,
+            MicroserviceClientSettings settings)
+            where TClient : class, IMicroserviceClient
+        {
+            if (settings == null)
+            {
+                throw new ArgumentNullException(nameof(settings), "Settings cannot be null.");
+            }
+
+            var clientName = typeof(TClient).FullName ?? Guid.NewGuid().ToString();
+
+            services.AddHttpClient(clientName, client =>
+            {
+                settings.HttpClientConfiguration?.Invoke(client);
+            });
+
+            // Register proxy for TClient
+            services.AddScoped<TClient>(provider =>
+            {
+                var httpClient = provider.GetRequiredService<IHttpClientFactory>().CreateClient(clientName);
+                return ProxyFactory<TClient>.Create(httpClient);
+            });
+
+            return services;
+        }
+    }
+}
---
+++ SilkRoute/Proxy/ProxyFactory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using SilkRoute.Interfaces;
+
+namespace SilkRoute.Proxy
+{
+    /// <summary>
+    /// Factory to create proxy instances.
+    /// </summary>
+    public static class ProxyFactory<T> where T : IMicroserviceClient
+    {
+        public static T Create(HttpClient httpClient)
+        {
+            object proxy = DispatchProxy.Create<T, Proxy<T>>();
+            ((Proxy<T>)proxy).SetHttpClient(httpClient);
+            return (T)proxy;
+        }
+    }
+}
